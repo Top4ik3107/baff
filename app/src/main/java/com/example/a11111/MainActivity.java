e06573e70fd26227c2e588083fc915e828009423
Top4ik3107 <dmironenko715@gmail.com>
2024-09-24 21:55:41 +0500

    yes

---
+++ app/src/main/java/com/example/a11111/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.a11111;
+
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView val;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        try {
+            val = findViewById(R.id.textView);
+            if (val != null) {
+                ObjectAnimator animation = ObjectAnimator.ofFloat(val, "translationY", 0f, -100f, 0f);
+                animation.setDuration(1000);
+                animation.setRepeatCount(ObjectAnimator.INFINITE);
+                animation.setRepeatMode(ObjectAnimator.REVERSE);
+                animation.start();
+            } else {
+                Log.e("MainActivity", "val is null");
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Логирование ошибки
+        }
+    }
+}
